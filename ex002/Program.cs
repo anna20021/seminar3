e28bdef316014de8563d6dda5eed96a0d07925ab
Снежана Павалаки <snezanapavalaki@MacBook-Air-Snezana.local>
2023-09-06 11:54:51 +0300

    solved ex2

---
+++ ex002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+
+a= number*number*number;
+
+Console.WriteLine(a);
